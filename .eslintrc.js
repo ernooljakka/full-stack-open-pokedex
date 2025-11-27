9bb7ae1b93bc31272a7f3f50c93199fd9c760989
Erno Oljakka <erno.oljakka@gmail.com>
2025-11-27 11:11:13 +0200

    fixed linting errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   "env": {
     "browser": true,
+    "node": true,
     "es6": true,
     "jest/globals": true
   },
@@ -21,15 +22,15 @@ module.exports = {
   "rules": {
     "indent": [
       "error",
-        2
-      ],
+      2
+    ],
     "linebreak-style": [
       "error",
       "unix"
     ],
     "quotes": [
-      "error",
-      "single"
+      'error',
+      'single'
     ],
     "semi": [
       "error",
